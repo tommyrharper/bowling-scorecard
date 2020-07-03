f7384abd5b2aaaf1ea47e50910fa3840e0c776e4
tommyrharper <tommyrharper@gmail.com>
2020-07-03 11:48:18 +0100

    Add first score test for Scorecard

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -9,5 +9,11 @@ describe('Scorecard,', function() {
     it('Starts with a score of 0 for 0 rolls', () => {
       expect(scorecard.score).toEqual(0);
     });
+
+    it('Score of 4 for rolls 1, 3', () => {
+      scorecard.addRoll(1);
+      scorecard.addRoll(3);
+      expect(scorecard.score).toEqual(4);
+    });
   });
 });
