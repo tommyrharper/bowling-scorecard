7384e8872e1d9a4087bd65b2a6b164cba809d5d4
tommyrharper <tommyrharper@gmail.com>
2020-07-03 15:49:13 +0100

    Correct test for triple strike to equal 60

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -31,12 +31,36 @@ describe('Scorecard,', function() {
       expect(scorecard.score).toEqual(18);
     });
 
+    it('Role a strike, strike, strike', () => {
+      scorecard.addRoll(10);
+      scorecard.addRoll(10);
+      scorecard.addRoll(10);
+      expect(scorecard.score).toEqual(60);
+    });
+
     it('Role a spare, strike, strike', () => {
       scorecard.addRoll(5);
       scorecard.addRoll(5);
       scorecard.addRoll(10);
       scorecard.addRoll(10);
       expect(scorecard.score).toEqual(40);
+    });
+
+    // it('Role a strike, strike, strike, strike', () => {
+    //   scorecard.addRoll(10);
+    //   scorecard.addRoll(10);
+    //   scorecard.addRoll(10);
+    //   scorecard.addRoll(10);
+    //   expect(scorecard.score).toEqual(50);
+    // });
+  });
+
+  describe('Full games', function() {
+    it('Perfect Game', () => {
+      for (let i = 0; i < 12; i++) {
+        scorecard.addRoll(10);
+      }
+      expect(scorecard.score).toEqual(300);
     });
   });
 });
