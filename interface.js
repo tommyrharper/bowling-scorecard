88fcceecac263b260b458a3773d5c573ad68ba58
tommyrharper <tommyrharper@gmail.com>
2020-07-03 19:48:05 +0100

    Save and update score for each frame in interface.js

--- interface.js
+++ interface.js
@@ -1,31 +1,67 @@
 $(document).ready(function() {
-  const scorecard = new Scorecard2();
+  const scorecard = new Scorecard();
   let numberOfRolls = 0;
 
   $('.record').click(function(e) {
     e.preventDefault(e);
+
     numberOfRolls += 1;
     const rollScore = parseInt(e.currentTarget.value);
     scorecard.addRoll(rollScore);
-    const frameNumber = scorecard.frames.length;
+    const frameNumber = scorecard.frame+1;
+    const frameIndex = scorecard.frame;
 
-    $('#score').text(scorecard.score);
-    $('#frames').text(frameNumber);
 
-    if (frameNumber >= 10 && rollScore === 10) {
-      $('#roll' + numberOfRolls).text(rollScore);
-    } else if (rollScore === 10) {
+    if (rollScore === 10) {
       $('#roll' + numberOfRolls).text(rollScore);
       numberOfRolls += 1;
       $('#roll' + numberOfRolls).text('/');
     } else {
       $('#roll' + numberOfRolls).text(rollScore);
-      $('#frame' + scorecard.frames.length).text;
+    }
+    
+    // $('#frame' + frameNumber).text(scorecard.frames[frameIndex].score);
+    console.log('scorecard.frames', scorecard.frames);
+    for (let i = 0; i < 10; i++) {
+      // console.log('scorecard.frames[i].type', scorecard.frames[i].type);
+      console.log('i', i);
+      if (scorecard.frames[i].type !== 'Incomplete') {
+        console.log('i', i);
+        console.log('INSIDE');
+        console.log('(i+1)', (i+1));
+        // $('#frame' + (i+1)).text("yo");
+        $('#frame' + (i+1)).text(scorecard.frames[i].score);
+        // break;
+      }
     }
 
-    updateButtons(scorecard.rolls.length, rollScore);
+    // This works DONT WORRY/IGNORE
+    $('#score').text(scorecard.score);
+    $('#frames').text(frameNumber);
 
-    $('#frame' + scorecard.frames.length).text(scorecard.score);
+
+    // numberOfRolls += 1;
+    // const rollScore = parseInt(e.currentTarget.value);
+    // scorecard.addRoll(rollScore);
+    // const frameNumber = scorecard.frame+1;
+
+    // $('#score').text(scorecard.score);
+    // $('#frames').text(frameNumber);
+
+    // if (frameNumber >= 10 && rollScore === 10) {
+    //   $('#roll' + numberOfRolls).text(rollScore);
+    // } else if (rollScore === 10) {
+    //   $('#roll' + numberOfRolls).text(rollScore);
+    //   numberOfRolls += 1;
+    //   $('#roll' + numberOfRolls).text('/');
+    // } else {
+    //   $('#roll' + numberOfRolls).text(rollScore);
+    //   $('#frame' + scorecard.frames.length).text;
+    // }
+
+    // updateButtons(scorecard.rolls.length, rollScore);
+
+    // $('#frame' + scorecard.frame+1).text(scorecard.score);
   });
 
   function updateButtons(rollsThisFrame, rollScore) {
