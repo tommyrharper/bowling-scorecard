c1dd07385776899ab5e21f4973a4b263f90da5de
tommyrharper <tommyrharper@gmail.com>
2020-07-03 11:48:42 +0100

    Add updateScore to Scorecard.js

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,5 +1,21 @@
 class Scorecard {
   constructor() {
     this.score = 0;
+    this.rolls = [];
+    this.frames = [];
+  }
+  addRoll(roll) {
+    this.rolls.push(roll);
+    if (this.rolls.length === 2) {
+      this.frames.push(new Frame(this.rolls[0], this.rolls[1]));
+      this.rolls = [];
+    }
+    this.updateScore();
+  }
+  updateScore() {
+    let index = 0;
+    for (index; index < this.frames.length; index++) {
+      this.score += this.frames[index].score;
+    }
   }
 }
