5eb9fc9674b8656fa2eaa23d3363fdb408ed12a2
tommyrharper <tommyrharper@gmail.com>
2020-07-03 14:31:01 +0100

    Refactor out addFirstRoll method in Frame.js

--- src/Frame.js
+++ src/Frame.js
@@ -5,18 +5,28 @@ class Frame {
     this.score = undefined;
   }
   addRoll(roll) {
-    console.log('roll', roll);
+    if (this.roll1 === undefined) {
+      this.addFirstRoll(roll);
+    } else if (this.roll2 === undefined) {
+      this.addSecondRoll(roll);
+    } else {
+      throw new Error('Frame already full');
+    }
+    this.updateFrameScore();
+  }
+  updateFrameScore() {
+    this.score = this.roll1 + this.roll2;
+  }
+  addFirstRoll(roll) {
     if (this.roll1 === undefined) {
       this.roll1 = roll;
       if (roll === 10) {
         this.roll2 = 0;
       }
-    } else if (this.roll2 === undefined) {
-      this.roll2 = roll;
-    } else {
-      throw new Error('Frame already full');
     }
-    this.score = this.roll1 + this.roll2;
+  }
+  addSecondRoll(roll) {
+    this.roll2 = roll;
   }
   isStrike() {
     if (this.roll1 === 10) {
