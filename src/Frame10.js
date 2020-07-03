7c00be410f4f107a19b149464ec63d7f4f2a6b42
tommyrharper <tommyrharper@gmail.com>
2020-07-03 15:56:22 +0100

    Add Frame code into Frame10

---
+++ src/Frame10.js
@@ -0,0 +1,41 @@
+class Frame10 {
+  constructor() {
+    this.type = 'Incomplete';
+    this.roll1 = undefined;
+    this.roll2 = undefined;
+    this.score = 0;
+  }
+  addRoll(roll) {
+    if (this.roll1 === undefined) {
+      this.addFirstRoll(roll);
+    } else if (this.roll2 === undefined) {
+      this.addSecondRoll(roll);
+    } else {
+      throw new Error('Frame already full');
+    }
+    this.updateFrameScore(roll);
+  }
+  updateFrameScore(roll) {
+    this.score += roll;
+  }
+  addFirstRoll(roll) {
+    if (this.roll1 === undefined) {
+      this.roll1 = roll;
+      if (roll === 10) {
+        this.roll2 = 0;
+        this.type = 'Strike';
+      }
+    }
+  }
+  addSecondRoll(roll) {
+    this.roll2 = roll;
+    if (this.roll1 + roll === 10) {
+      this.type = 'Spare';
+    } else {
+      this.type = 'OpenFrame';
+    }
+  }
+  addBonus(bonus) {
+    this.score += bonus;
+  }
+}
