b9de3fbd202188c2b6b156deb6424d7d509ada17
tommyrharper <tommyrharper@gmail.com>
2020-07-03 14:26:52 +0100

    Add addRoll method to Frame.js

--- src/Frame.js
+++ src/Frame.js
@@ -1,8 +1,22 @@
 class Frame {
-  constructor(roll1, roll2 = 0) {
-    this.roll1 = roll1;
-    this.roll2 = roll2;
-    this.score = roll1 + roll2;
+  constructor() {
+    this.roll1 = undefined;
+    this.roll2 = undefined;
+    this.score = undefined;
+  }
+  addRoll(roll) {
+    console.log('roll', roll);
+    if (this.roll1 === undefined) {
+      this.roll1 = roll;
+      if (roll === 10) {
+        this.roll2 = 0;
+      }
+    } else if (this.roll2 === undefined) {
+      this.roll2 = roll;
+    } else {
+      throw new Error('Frame already full');
+    }
+    this.score = this.roll1 + this.roll2;
   }
   isStrike() {
     if (this.roll1 === 10) {
