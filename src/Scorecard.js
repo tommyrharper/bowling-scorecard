b4d300c9358fa74410ee5e4884ac232d3985b93f
tommyrharper <tommyrharper@gmail.com>
2020-07-03 15:06:20 +0100

    Add solution using 10 frames already in the frames array

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,21 +1,42 @@
 class Scorecard {
   constructor() {
+    this.totalRolls = 0;
     this.score = 0;
+    this.frames = [
+      new Frame(),
+      new Frame(),
+      new Frame(),
+      new Frame(),
+      new Frame(),
+      new Frame(),
+      new Frame(),
+      new Frame(),
+      new Frame(),
+      new Frame(),
+    ];
     this.rolls = [];
-    this.frames = [];
   }
   addRoll(roll) {
-    this.rolls.push(roll);
-    if (this.rolls.length === 2) {
-      this.frames.push(new Frame(this.rolls[0], this.rolls[1]));
-      this.rolls = [];
+    // console.log('this.frames', this.frames);
+    console.log('roll', roll);
+    // console.log('this.frames before loop', this.frames);
+    for (let i = 0; i < 10; i++) {
+      if (this.frames[i].type === 'Incomplete') {
+        // console.log('i', i);
+        this.frames[i].addRoll(roll);
+        break;
+      }
     }
+    // console.log('this.frames', this.frames);
     this.updateScore();
   }
   updateScore() {
-    let index = 0;
-    for (index; index < this.frames.length; index++) {
-      this.score += this.frames[index].score;
+    this.score = 0;
+    for (let i = 0; i < 10; i++) {
+      console.log('i', i);
+      // console.log('this.frames[i].score', this.frames[i].score);
+      console.log('this.frames', this.frames);
+      this.score += this.frames[i].score;
     }
   }
 }
